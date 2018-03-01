1967d2b1524753e16e9fdac58f7084ff4f99a215
Kenneth Santos <kenneth.santos@gt4w.com.br>
2018-03-01 18:10:09 -0300

    criar estrutura inicial de reactive forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,28 +1,21 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
-import { NgForm } from '@angular/forms';
 
 @Component({
-	selector: 'app-ordem-compra',
-	templateUrl: './ordem-compra.component.html',
-	styleUrls: ['./ordem-compra.component.css'],
-	providers: [ OrdemCompraService ]
+  selector: 'app-ordem-compra',
+  templateUrl: './ordem-compra.component.html',
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
-	idPedidoCompra: number
-	@ViewChild('formulario') f: NgForm
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
-	constructor(private ordemCompraService: OrdemCompraService) { }
+  ngOnInit() {
+    
+  }
 
-	ngOnInit() {
-
-	}
-
-	confirmarCompra() {
-		this.ordemCompraService.efetivarCompra(this.f.value)
-			.subscribe((id: number) => this.idPedidoCompra = id)
-	}
-
+  public confirmarCompra(): void {
+  }
 }
